c069acff596fd9b1c1b432ca809dc67d2df3a270
ndev <napodevesa@gmail.com>
2020-11-20 22:57:04 +0100

    temp monitor

---
+++ monitortemp_7c095dc6-54a2-4e8a-8cca-1a3585a4fde7.ino
@@ -0,0 +1,30 @@
+#include<LiquidCrystal.h>
+int tempC;
+int tempCOld;
+const int sensorTemp = A0;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+Serial.begin(9600);
+lcd.begin(16,2);
+}
+
+void loop() {
+
+tempC = analogRead(sensorTemp);
+tempC = tempC*.48828125;
+Serial.println(tempC);
+lcd.setCursor(0,0);
+lcd.print("Temperatura:");
+lcd.setCursor(0,1);
+lcd.print(tempC);
+delay(1000);
+lcd.clear();
+lcd.setCursor(0,0);
+lcd.print("Temperature:");
+lcd.setCursor(0,1);
+lcd.print(tempC);
+delay(1000);
+lcd.clear();
+}
+
